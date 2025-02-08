21a9ee2f1da3f2287aa75e9ece216726c9fd91c5
“KazukiKoike” <devkazukikoike7788@gmail.com>
2025-02-08 23:53:35 +0900

    revert testEnvironment setting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: process.env.NODE_ENV === 'ci' ? 'node' : 'jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
