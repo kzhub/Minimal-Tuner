558cc3351a6a7b234bfe40cd76030f4ec9003933
SHG9-hub <zhuanzhitailang74@gmail.com>
2025-03-30 13:30:27 +0900

    両言語ヘッド内に'noindex'が反映されていることを確認。それに伴うテストファイルの削除。

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
       tsconfig: 'tsconfig.test.json',
     },
   },
-  // スタイルファイルとアセットファイルのモック設定
+  // モジュールモック設定
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.ts",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/node_modules/jest-transform-stub",
     "^@/(.*)$": "<rootDir>/src/$1"
   },
   testPathIgnorePatterns: [
