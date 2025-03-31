797a456635b7f09ad945af5ffccc4a4a51e0a3e6
“KazukiKoike” <devkazukikoike7788@gmail.com>
2025-04-01 03:31:36 +0900

    ヘッダーにGoogle Adsのスクリプトを追加。広告表示のための設定を行いました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import { translations, DEFAULT_LOCALE } from "@/lib/constants";
+import Script from "next/script";
 
 const ubuntu = Ubuntu({
   weight: "700",
@@ -41,6 +42,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang={DEFAULT_LOCALE}>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2543187308039193"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={ubuntu.className}>{children}</body>
     </html>
   );
